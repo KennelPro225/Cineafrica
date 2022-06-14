3442135be4c2a337c7000a0e68e95b9523c5ccfb
KennelPro225 <101474670+KennelPro225@users.noreply.github.com>
2022-06-14 12:28:42 +0000

    Update validationform.js

--- src/components/validationform.js
+++ src/components/validationform.js
@@ -1,10 +1,1 @@
-import React from "react";
 
-export default class ValidationForm extends React.Component{
-    state = {
-        name="",
-        email="",
-        password="",
-        nameError="",
-    }
-}
